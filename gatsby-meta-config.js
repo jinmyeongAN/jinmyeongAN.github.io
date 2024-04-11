ea4db9418788feb5ba998817f2c7df60c9e112d9
jinmyeongAN <jinmyeong.an@gmail.com>
2024-04-11 16:39:12 +0900

    feat: test to modify the info

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `https://jinmyeongan.github.io/`,
+  description: `Jimmy의 개발여행`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://jinmyeongan.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,10 +11,10 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `안진명`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['이로운 것을 만드는', '나의 일을 사랑하는', '함께 나아가고 싶은'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
